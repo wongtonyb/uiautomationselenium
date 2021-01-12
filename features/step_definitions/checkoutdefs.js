302d161acb911852b58bebf4f9cc66bd2bc6556a
tony <wongtonyb@gmail.com>
2021-01-11 23:38:24 -0500

    commenting

--- features/step_definitions/checkoutdefs.js
+++ features/step_definitions/checkoutdefs.js
@@ -1,74 +1,77 @@
-// const { Given, When, Then, setDefaultTimeout } = require("@cucumber/cucumber");
-// const { Builder, By, Capabilities, Key } = require("selenium-webdriver");
-// const assert = require("assert");
-// setDefaultTimeout(60 * 1000);
+const { Given, When, Then, setDefaultTimeout } = require("@cucumber/cucumber");
+const { Builder, By, Capabilities, Key } = require("selenium-webdriver");
+const assert = require("assert");
+setDefaultTimeout(60 * 1000);
 
-// require("chromedriver");
+require("chromedriver");
 
-// // driver setup
+// driver setup
 
-// const capabilities = Capabilities.chrome();
-// capabilities.set("chromeOptions", { w3c: false });
-// const driver = new Builder().withCapabilities(capabilities).build();
+const capabilities = Capabilities.chrome();
+capabilities.set("chromeOptions", { w3c: false });
+const driver = new Builder().withCapabilities(capabilities).build();
 
-// let cart = [];
+let cart = [];
 
-// Given("User is on inventory page", async function () {
-//   await driver.get("https://www.saucedemo.com/inventory");
-// });
+Given("User is on inventory page", async function () {
+  await driver.get("https://www.saucedemo.com/inventory");
+});
 
-// Then("User adds {string}", async function (item) {
-//   await driver
-//     .findElement(
-//       By.xpath(
-//         `//div[text()='${item}']//parent::*/parent::*/parent::*/div[3]/button`
-//       )
-//     )
-//     .click();
-//   cart.push(item);
-// });
+Then("User adds {string}", async function (item) {
+  await driver
+    .findElement(
+      By.xpath(
+        `//div[text()='${item}']//parent::*/parent::*/parent::*/div[3]/button`
+      )
+    )
+    .click();
+  cart.push(item);
+});
 
-// Then("User clicks on cart", async function () {
-//   await driver
-//     .findElement(By.xpath('//*[@id="shopping_cart_container"]/a'))
-//     .click();
-// });
+Then("User clicks on cart", async function () {
+  await driver
+    .findElement(By.xpath('//*[@id="shopping_cart_container"]/a'))
+    .click();
+});
 
-// Then("User redirects to cart page with correct items", function () {
-//   while (cart.length) {
-//     driver.findElement(By.xpath(`//div[text()='${cart.pop()}']`));
-//   }
-// });
+Then("User redirects to cart page with correct items", function () {
+  while (cart.length) {
+    driver.findElement(By.xpath(`//div[text()='${cart.pop()}']`));
+  }
+});
 
-// Then("User clicks on Checkout button", async function () {
-//   await driver.findElement(By.xpath(`//a[text()='CHECKOUT']`)).click();
-// });
+Then("User clicks on Checkout button", async function () {
+  await driver.findElement(By.xpath(`//a[text()='CHECKOUT']`)).click();
+});
 
-// Then(
-//   "User fills out checkout form with {string}, {string}, and {string}",
-//   async function (firstName, lastName, zipCode) {
-//     await driver.findElement(By.id("first-name")).sendKeys(firstName);
-//     await driver.findElement(By.id("last-name")).sendKeys(lastName);
-//     await driver.findElement(By.id("postal-code")).sendKeys(zipCode);
-//   }
-// );
+Then(
+  "User fills out checkout form with {string}, {string}, and {string}",
+  async function (firstName, lastName, zipCode) {
+    await driver.findElement(By.id("first-name")).sendKeys(firstName);
+    await driver.findElement(By.id("last-name")).sendKeys(lastName);
+    await driver.findElement(By.id("postal-code")).sendKeys(zipCode);
+  }
+);
 
-// Then("User clicks on Continue button", async function () {
-//   await driver.findElement(By.xpath("//input[@value='CONTINUE']")).click();
-// });
+Then("User clicks on Continue button", async function () {
+  await driver.findElement(By.xpath("//input[@value='CONTINUE']")).click();
+});
 
-// Then(
-//   "User reviews {string} completes checkout by clicking Finish button",
-//   async function (expectedTotalPrice) {
-//     // let e = await driver.findElement(
-//     //   By.xpath("/div[@class='summary_subtotal_label']/text()[2]")
-//     //   //   By.xpath("/div[@class='summary_subtotal_label']")
-//     // );
+/*
+attempted to get the price text from div tab for assertion comparison
+*/
+Then(
+  "User reviews {string} completes checkout by clicking Finish button",
+  async function (expectedTotalPrice) {
+    // let e = await driver.findElement(
+    //   By.xpath("/div[@class='summary_subtotal_label']/text()[2]")
+    //   //   By.xpath("/div[@class='summary_subtotal_label']")
+    // );
 
-//     // let actualTotalPrice = await e.getText();
+    // let actualTotalPrice = await e.getText();
 
-//     // assert.strictEqual(actualTotalPrice, expectedTotalPrice);
+    // assert.strictEqual(actualTotalPrice, expectedTotalPrice);
 
-//     await driver.findElement(By.xpath("//a[text()='FINISH']")).click();
-//   }
-// );
+    await driver.findElement(By.xpath("//a[text()='FINISH']")).click();
+  }
+);
